e75cecb733b8ae0b097befceb8f85f506e2ec269
odilsonjs <italisodilsonwoodly@hotmail.com>
2024-08-12 22:28:51 -0400

    Update function updateItemGET

--- controllers/itemsController.js
+++ controllers/itemsController.js
@@ -2,7 +2,7 @@ const db = require("../db/queries");
 
 async function createItemsPOST(req, res) {
   db.insertItem(req.body);
-  res.send(`Item: ${req.body}`);
+  res.redirect("/items");
 }
 
 async function createItemsGET(req, res) {
@@ -12,8 +12,16 @@ async function createItemsGET(req, res) {
 
 async function getItems(req, res) {
   const items = await db.getAllItems();
-  console.log("items: ", items);
-  res.send("items: " + items.map((user) => user.username).join(", "));
+
+  res.render("itemsIndex", { items });
 }
 
-module.exports = { getItems, createItemsGET, createItemsPOST };
+async function updateItemGET(req, res) {
+  const { id } = req.params;
+
+  const item = await db.getItemWithId(id);
+  const categories = await db.getAllCategories();
+
+  res.render("itemsUpdate", { item, categories });
+}
+module.exports = { getItems, createItemsGET, createItemsPOST, updateItemGET };
