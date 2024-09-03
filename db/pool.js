60c8b399242754203f32c326a64bd569b176b513
odilsonjs <italisodilsonwoodly@hotmail.com>
2024-09-03 15:50:54 -0400

    Update pool

--- db/pool.js
+++ db/pool.js
@@ -2,5 +2,5 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 module.exports = new Pool({
-  connectionString: process.env.DATABASE_URI,
+  connectionString: process.env.DATABASE_URL,
 });
